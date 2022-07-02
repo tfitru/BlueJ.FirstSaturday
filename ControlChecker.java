ae9c99bf0af4f532d09711264d1827ee5f4b3241
Fitru <fitru@Zipcoders-MacBook-Pro-100.local>
2022-07-02 15:47:24 -0400

    Added instance threshold and static threshold

---
+++ ControlChecker.java
@@ -0,0 +1,50 @@
+/**
+ * @author git-leon
+ * @version 1.0.0
+ * @date 5/19/21 11:02 AM
+ */
+
+public class ControlChecker {
+
+
+
+        /**
+         * Write a method that returns "more heat!"
+         * if `roomTemperature` is less than `70`,
+         * else returns "less heat!"
+         */
+        public String getThermostatControls(int roomTemperature) {
+            if(roomTemperature < 70) {
+                return "more heat!";
+            }
+            return "less heat!";
+        }
+
+        /**
+         * Write a method that returns "refuel"
+         * if `fuelLevel` is less than 0.08
+         * else returns "burn fuel"
+         */
+        public String getFuelControls(double fuelLevel) {
+            if(fuelLevel<=0.08){
+                return "refuel";
+            } else
+                return "burn fuel";
+
+        }
+
+        /**
+         * Write a method that returns "start fireplace!"
+         * if `outsideTemperature` is less than `50` and
+         *    `insideTemperature`  is less than `60`
+         * else returns "stop fireplace!"
+         */
+        public String getFireplaceControls(int outsideTemperature, int insideTemperature) {
+            if(outsideTemperature < 50 && insideTemperature < 60) {
+                return "start fireplace!";
+            } else {
+                return "stop fireplace!";
+            }
+        }
+    }
+
---
+++ InstancedThresholdEvaluatorTest.java
@@ -0,0 +1,50 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+public class InstancedThresholdEvaluatorTest {
+    // given
+    @Test
+    public void testSetThreshold() {
+        for (Integer thresholdToBeSet : new Integer[]{0, 5, 10, Integer.MAX_VALUE, Integer.MIN_VALUE}) {
+            int expected = thresholdToBeSet;
+            InstancedThresholdEvaluator thresholdEvaluator = new InstancedThresholdEvaluator();
+
+            // when
+            thresholdEvaluator.setThreshold(thresholdToBeSet);
+            int actual = thresholdEvaluator.getThreshold();
+
+            // then
+            Assert.assertEquals(expected, actual);
+        }
+    }
+
+    // given
+    @Test
+    public void testIsThresholdExceedingTrue() {
+        for (Integer thresholdTobeSet : new Integer[]{0, 5, 10, Integer.MIN_VALUE}) {
+            testIsThresholdExceeding(thresholdTobeSet, 1, false);
+        }
+    }
+
+    // given
+    @Test
+    public void testIsThresholdExceedingFalse() {
+        for (Integer thresholdToBeSet : new Integer[]{0, 5, 10, Integer.MAX_VALUE}) {
+            testIsThresholdExceeding(thresholdToBeSet, -1, true);
+        }
+    }
+
+    // given
+    private void testIsThresholdExceeding(int thresholdToBeSet, int thresholdOffset, boolean expected) {
+        Integer valueToCompareAgainstThreshold = thresholdToBeSet + thresholdOffset;
+        InstancedThresholdEvaluator thresholdEvaluator = new InstancedThresholdEvaluator();
+        thresholdEvaluator.setThreshold(thresholdToBeSet);
+
+        // when
+        boolean actual = thresholdEvaluator.isThresholdExceeding(valueToCompareAgainstThreshold);
+
+        // then
+        Assert.assertEquals(expected, actual);
+    }
+}
+
